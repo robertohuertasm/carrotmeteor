a84a3564ddb4d1ccb5bdf30e8d7f4b2098d4c581
Roberto Huertas <roberto.huertas@outlook.com>
2016-08-09 23:56:46 +0200

    4.4.7 added

---
+++ 4.4.7/start.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir /tmp/meteor
+curl -SL https://install.meteor.com/ -o /tmp/meteor/inst 
+echo "WE'VE GOT METEOR SH"
+sed -e "s/^RELEASE=.*/RELEASE=\"\$METEOR_VERSION\"/" /tmp/meteor/inst > /tmp/meteor/inst-canonical 
+chmod +x /tmp/meteor/inst 
+/tmp/meteor/inst 
+rm -rf /tmp/meteor
+npm install -g mupx
